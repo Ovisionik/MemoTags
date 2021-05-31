70d20631931a227971cb090b50f7fd890d91274b
Ovisionik <Ovisionik@gmail.com>
2021-05-31 23:37:20 +0200

    Add project files.

---
+++ MemoTags/MemoTags/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MemoTags.Models;
+
+namespace MemoTags.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
